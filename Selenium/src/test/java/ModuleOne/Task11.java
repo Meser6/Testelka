f9f24e2a98948fc042537a2eeb1d3464c3607e5c
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2020-09-23 11:18:06 +0200

    Task 1.1

--- Selenium/src/test/java/ModuleOne/Task11.java
+++ Selenium/src/test/java/ModuleOne/Task11.java
@@ -1,4 +1,46 @@
 package ModuleOne;
 
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task11 {
+
+    WebDriver driver;
+    String wikiTitle = "Wikipedia, wolna encyklopedia";
+    String nasaTitle = "NASA";
+
+    @BeforeEach
+    public void chromeSettings(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().fullscreen();
+        driver.get("https://pl.wikipedia.org/");
+        driver.get("https://www.nasa.gov/");
+    }
+
+    @AfterEach
+    public void closeDriver(){
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void correctMoving(){
+        driver.navigate().back();
+        Assertions.assertEquals(wikiTitle, driver.getTitle(), "Page title is not" + wikiTitle);
+
+        driver.navigate().forward();
+        Assertions.assertEquals(nasaTitle, driver.getTitle(), "Page title is not" + nasaTitle);
+    }
+
+    @Test
+    public void incorrectMoving(){
+        driver.navigate().back();
+        Assertions.assertNotEquals(nasaTitle, driver.getTitle());
+
+        driver.navigate().forward();
+        Assertions.assertNotEquals(wikiTitle, driver.getTitle());
+
+    }
 }
